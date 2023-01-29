5576d5f36b5e695a90b99875771d6c6208c13aad
Ivan Novakov <inovakov.bg@gmail.com>
2023-01-29 12:41:58 +0200

    Fix

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,100 +2,82 @@ import EventEmitter from "eventemitter3";
 import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
+  static get events() {
+    return {
+      READY: "ready",
+    };
+  }
 
-    static get events() {
-        return {
-            READY: "ready",
-        };
-    }
+  _loading = document.querySelector(".progress");
+  _startLoading() {
+    this._loading.style.visibility = "visible";
+  }
+  _stopLoading() {
+    this._loading.style.display = "none";
+    console.log("STOP")
+  }
 
-    constructor() {
+  constructor() {
+    super();
+    this._load();
+    this.emit(Application.events.READY);
+  }
 
-        super();
+  async _load() {
+    this._startLoading();
+    let pageNumber = 1;
 
-        this._loading = document.querySelector("progress");
-        this._startLoading();
+    // Fetch planets
+    let planets = [];
+    let response;
+    let data;
+    do {
+      if (pageNumber == 1) {
+        response = await fetch("https://swapi.boom.dev/api/planets");
+      } else {
+        response = await fetch("https://swapi.boom.dev/api/planets?page=" + pageNumber);
+      }
+      data = await response.json();
+      planets = planets.concat(data.results);
+      console.log(planets)
+      pageNumber++;
+    } while (data.next);
 
-        this.emit(Application.events.READY);
-    }
+    // Render planets
+    planets.forEach((planet) => {
+      this._create(planet);
+    });
 
+    this._stopLoading();
+  }
 
-    _startLoading() {
-        this._loading.style.visibility = "visible";
-        this._load();
-    }
+  _create(planet) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.innerHTML = this._render(planet);
 
-    _stopLoading() {
-        this._loading.style.visibility = "hidden";
-    }
+    document.body.querySelector(".main").appendChild(box);
+  }
 
-    async _load() {
-
-        let url = await "https://swapi.boom.dev/api/planets";
-
-        function checkStatus(responce) {
-            if (responce.status >= 200 && responce.status < 300) {
-                return Promise.resolve(responce);
-            } else {
-                return Promise.reject(new Error(responce.statusText));
-            }
-        }
-
-        function toJSON(responce) {
-            return responce.json();
-        }
-
-        for (let i = 1; i <= 6; i++) {
-            console.log(url);
-            if (i > 1) {
-                url = 'https://swapi.boom.dev/api/planets?page=' + i;
-            }
-            fetch(url)
-                .then(checkStatus)
-                .then(toJSON)
-                .then((data) => {
-                    data.results.forEach(d => {
-                        this._create(d.name, d.terrain, d.population);
-                    })
-                })
-        }
-        this._stopLoading();
-
-
-    }
-
-    _create(name, terrain, population) {
-        if (name !== undefined) {
-            const box = document.createElement("div");
-            box.classList.add("box");
-            box.innerHTML = this._render({
-                name: name,
-                terrain: terrain,
-                population: population,
-            });
-            document.body.querySelector(".main").appendChild(box);
-        }
-    }
-
-
-    _render({name, terrain, population}) {
-        return `
-          <article class="media">
-            <div class="media-left">
-              <figure class="image is-64x64">
-                <img src="${image}" alt="planet">
-              </figure>
-            </div>
-            <div class="media-content">
-              <div class="content">
-              <h4>${name}</h4>
-                <p>
-                  <span class="tag">${terrain}</span> <span class="tag">${population}</span>
-                  <br>
-                </p>
-              </div>
-            </div>
-          </article>
+  _render({ name, terrain, population, url }) {
+    return `
+<article class="media">
+  <div class="media-left">
+    <figure class="image is-64x64">
+      <img src="${image}" alt="planet">
+    </figure>
+  </div>
+  <div class="media-content">
+    <div class="content">
+    <h4>${name}</h4>
+      <p>
+        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
+        <br>
+      </p>
+      <p class="tag">url:<a href="${url}" target="_blank">${url}</a></p>
+    </div>
+  </div>
+</article>
     `;
-    }
-}
+  }
+ }
