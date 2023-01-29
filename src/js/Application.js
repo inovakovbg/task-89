3bd52f1a25e30d86321fc8f205ba1700d90b2dab
Ivan Novakov <inovakov.bg@gmail.com>
2023-01-29 12:48:22 +0200

    F1

--- src/js/Application.js
+++ src/js/Application.js
@@ -8,23 +8,58 @@ export default class Application extends EventEmitter {
     };
   }
 
+  _loading = document.querySelector(".progress");
+  _startLoading() {
+    this._loading.style.visibility = "visible";
+  }
+  _stopLoading() {
+    this._loading.style.display = "none";
+    console.log("STOP")
+  }
+
   constructor() {
     super();
-
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
-    });
-
-    document.body.querySelector(".main").appendChild(box);
-
+    this._load();
     this.emit(Application.events.READY);
   }
 
-  _render({ name, terrain, population }) {
+  async _load() {
+    this._startLoading();
+    let pageNumber = 1;
+
+    // Fetch planets
+    let planets = [];
+    let response;
+    let data;
+    do {
+      if (pageNumber == 1) {
+        response = await fetch("https://swapi.boom.dev/api/planets");
+      } else {
+        response = await fetch("https://swapi.boom.dev/api/planets?page=" + pageNumber);
+      }
+      data = await response.json();
+      planets = planets.concat(data.results);
+      console.log(planets)
+      pageNumber++;
+    } while (data.next);
+
+    // Render planets
+    planets.forEach((planet) => {
+      this._create(planet);
+    });
+
+    this._stopLoading();
+  }
+
+  _create(planet) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.innerHTML = this._render(planet);
+
+    document.body.querySelector(".main").appendChild(box);
+  }
+
+  _render({ name, terrain, population, url }) {
     return `
 <article class="media">
   <div class="media-left">
@@ -39,9 +74,10 @@ export default class Application extends EventEmitter {
         <span class="tag">${terrain}</span> <span class="tag">${population}</span>
         <br>
       </p>
+      <p class="tag">url:<a href="${url}" target="_blank">${url}</a></p>
     </div>
   </div>
 </article>
     `;
   }
-}
+ }
